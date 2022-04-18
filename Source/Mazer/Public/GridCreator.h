af2e18963e642c60aca96dd828a8d72b8b29e870
KSydoruk <Konstantyn.Sydoruk@intusurg.com>
2022-04-18 17:59:23 +0300

    removed comments

--- Source/Mazer/Public/GridCreator.h
+++ Source/Mazer/Public/GridCreator.h
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -23,7 +21,6 @@ class MAZER_API AGridCreator : public AActor
 {
 	GENERATED_BODY()
 public:	
-	// Sets default values for this actor's properties
 	AGridCreator(const FObjectInitializer& ObjectInitializer);
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Grid")
